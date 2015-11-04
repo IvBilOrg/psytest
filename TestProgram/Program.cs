c4f1a99a992154e157aa75ec9d16f2a28603bf2a
Ivan Biletskiy <vanox8@gmail.com>
2015-11-04 16:14:30 +0200

    создание файла README

---
+++ TestProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            TxtTest txtTest = new TxtTest("D:\\Ivan\\Учеба\\Кононов лабы\\ПРОЕКТЫ\\TestProgram\\TestProgram\\TextFile.txt");
+
+            TxtTest txtTest = new TxtTest("TextFile.txt");
+            txtTest.Show();
+            Console.ReadLine();
+        }
+
+
+    }
+}
